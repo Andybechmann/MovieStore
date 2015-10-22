84d7c60c31536aec1d2aace3ac7618b9fa17ef6a
Anderzz <Anders_1502@hotmail.com>
2015-10-22 14:12:02 +0200

    mindre rettelser

--- MovieStore/MovieStoreDAL/MovieStoreDbInitializer.cs
+++ MovieStore/MovieStoreDAL/MovieStoreDbInitializer.cs
@@ -11,13 +11,13 @@ namespace MovieStoreDAL
     {
         protected override void Seed(MovieStoreDbContext context)
         {
-            Movie movie1 = new Movie { Title = "Star Wars", Year = DateTime.Now, ImageUrl = "http://ia.media-imdb.com/images/M/MV5BMTkwNzAwNDA4N15BMl5BanBnXkFtZTgwMTA2MDcwNzE@._V1__SX1857_SY903_.jpg", TrailerUrl = "https://www.youtube.com/embed/sGbxmsDFVnE", Price = 15 };
-            Movie movie = new Movie { Title = "Batman",Year=DateTime.Now, ImageUrl = "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1__SX1857_SY903_.jpg", TrailerUrl = "https://www.youtube.com/embed/neY2xVmOfUM", Price = 10 };
+            Movie movie1 = new Movie { Title = "Star Wars", Year = DateTime.Now, ImageUrl = "http://ia.media-imdb.com/images/M/MV5BMTkwNzAwNDA4N15BMl5BanBnXkFtZTgwMTA2MDcwNzE@._V1__SX1857_SY903_.jpg", TrailerUrl = "https://www.youtube.com/embed/sGbxmsDFVnE", Price = 100 };
+            Movie movie = new Movie { Title = "Batman", Year=DateTime.Now, ImageUrl = "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1__SX1857_SY903_.jpg", TrailerUrl = "https://www.youtube.com/embed/neY2xVmOfUM", Price = 250 };
             OrderLine orderline = new OrderLine() { Movie = movie1, Amount = 4 };
             OrderLine orderline1 = new OrderLine() { Movie = movie, Amount = 12 };
             Customer customer = new Customer
             {
-                FirstName = "Karl", Address = new Address() { Streetname = "ostegade" },
+                FirstName = "Karl", LastName = "Karlsen", Address = new Address() { Streetname = "Østergade"  },
                 Email = "andy@andy"
             };
             Order order = new Order { Date = DateTime.Now, OrderLines = new List<OrderLine>() {orderline,orderline1 }, Customer = customer };
